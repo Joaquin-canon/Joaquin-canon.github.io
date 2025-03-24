de19d41a042628701d8994b934b0dbc8f1787fd9
Deivit Agudelo <deivit.agu7@hotmail.com>
2025-03-24 14:54:21 -0500

    Delete JS directory

--- JS/eventos.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sidebar = document.getElementById('sidebar');
-    const sidebarToggle = document.getElementById('sidebarToggle');
-
-    sidebarToggle.addEventListener('click', function () {
-        sidebar.classList.toggle('collapsed'); // Alternar la clase "collapsed"
-    });
-});
